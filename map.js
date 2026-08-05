575f636105f5d5868331d3dc72ad5ba1a01a9a8d
oksy6220-hub <oksy6220@gmail.com>
2026-08-05 05:58:04 +0300

    Update map.js

--- map.js
+++ map.js
@@ -4,6 +4,12 @@ const svg = svgObject.contentDocument;
 
 console.log(svg.documentElement);
 
+const elements = svg.querySelectorAll("[id]");
+
+elements.forEach(el => {
+    console.log(el.id);
+});
+
 svgObject.addEventListener("load", function () {
 
     console.log("SVG загружен!");
