08125a8b5049304ca04cb656144fc7608fa3e46d
oksy6220-hub <oksy6220@gmail.com>
2026-08-05 05:14:54 +0300

    Update map.js

--- map.js
+++ map.js
@@ -1,1 +1,19 @@
+// Ждем загрузки SVG-карты
+window.addEventListener("load", () => {
 
+    const object = document.getElementById("svg-map");
+
+    object.addEventListener("load", () => {
+
+        const svg = object.contentDocument;
+
+        if (!svg) {
+            console.error("SVG не найден");
+            return;
+        }
+
+        console.log("SVG успешно загружен");
+
+    });
+
+});
