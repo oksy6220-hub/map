ceff38b4b540dba5a385fa83d6e0a26b665d1e9e
oksy6220-hub <oksy6220@gmail.com>
2026-08-05 06:13:36 +0300

    Update map.js

--- map.js
+++ map.js
@@ -1,9 +1,15 @@
 const svgObject = document.getElementById("svg-map");
 
-svgObject.addEventListener("load", function () {
+svgObject.addEventListener("load", () => {
 
     const doc = svgObject.contentDocument;
 
-    console.log(doc.querySelector("#moscow"));
+    console.log("=== Проверка ===");
+
+    console.log("getElementById:", doc.getElementById("moscow"));
+
+    console.log("querySelector:", doc.querySelector("#moscow"));
+
+    console.log("Все path:", doc.querySelectorAll("path").length);
 
 });
